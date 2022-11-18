4340e4ade392f7ec2534f3490e7291ede76fc62c
JongSub <JongSub@DESKTOP-VS9K4SA>
2022-11-18 13:57:27 +0900

    초기 커밋.

---
+++ DX3D_2211/framework.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "targetver.h"
+#define WIN32_LEAN_AND_MEAN
+
+#define WIN_WIDTH 1280
+#define WIN_HEIGTH 720
+
+// Windows 헤더 파일
+#include <windows.h>
+
+// DirectX 11
+#include <d3d11.h>
+
+#pragma comment(lib, "d3d11.lib");
